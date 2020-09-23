26a162deb1369f5a461ae8c7eac516b27dc6925b
Jack O'Sullivan <jackos1998@gmail.com>
2020-09-23 18:05:22 +0100

    Flush iptables on startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+iptables -F
+iptables -t nat -F
 if [ -n "$ROUTER_NETWORK" ]; then
     iptables -A FORWARD -s "$ROUTER_NETWORK" -d 10.0.0.0/8,172.16.0.0/12,192.168.0.0/16 -j DROP
     iptables -t nat -A POSTROUTING -s "$ROUTER_NETWORK" -j SNAT --to-source "$(hostname -i)"
