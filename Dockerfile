b7819d111aa2f9ade6493efec1648ec2aaddfeb9
Jack O'Sullivan <jackos1998@gmail.com>
2020-09-23 18:00:44 +0100

    Add routing

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM alpine:3.12
 
-RUN apk --no-cache add dnsmasq && \
+RUN apk --no-cache add iptables dnsmasq && \
     mkdir /var/lib/dnsmasq
 
 COPY entrypoint.sh /
 
-ENV DEBUG=no
+ENV DEBUG=no \
+    ROUTER_NETWORK=
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 
+if [ -n "$ROUTER_NETWORK" ]; then
+    iptables -A FORWARD -s "$ROUTER_NETWORK" -d 10.0.0.0/8,172.16.0.0/12,192.168.0.0/16 -j DROP
+    iptables -t nat -A POSTROUTING -s "$ROUTER_NETWORK" -j SNAT --to-source "$(hostname -i)"
+fi
+
 ARGS="--conf-file=/run/config/dnsmasq.conf"
 dnsmasq $ARGS --test
 
